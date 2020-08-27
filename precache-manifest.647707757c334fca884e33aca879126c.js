028704171de12895dc765fb609d12721e06ddac5
vinayak-gaikwad <58037628+vinayak-gaikwad@users.noreply.github.com>
2020-08-27 19:29:08 +0530

    Updates

---
+++ precache-manifest.647707757c334fca884e33aca879126c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b149a48983726ba4114ec6d456da8cb2",
+    "url": "/quotes_app/index.html"
+  },
+  {
+    "revision": "250795ec6d8c25556306",
+    "url": "/quotes_app/static/css/main.7fff5b8b.chunk.css"
+  },
+  {
+    "revision": "a524c323a103650b40c7",
+    "url": "/quotes_app/static/js/2.3b492db1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quotes_app/static/js/2.3b492db1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "250795ec6d8c25556306",
+    "url": "/quotes_app/static/js/main.5d83463d.chunk.js"
+  },
+  {
+    "revision": "fbe663309855522b4aac",
+    "url": "/quotes_app/static/js/runtime-main.41740cee.js"
+  }
+]);
